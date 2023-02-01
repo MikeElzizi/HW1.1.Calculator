b3b6f60cf5e6ec5a7012e10465091fa6e6771d6f
Mike <aplinesklaris@gmail.com>
2023-02-01 16:31:43 +0300

    Merge

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         int c = calc.devide.apply(a, b); //было деление на ноль, отсюда и была ошибка.
         // Подправил переменную devide в классе Calculator
+        errorCheck(c);
         calc.println.accept(c);
+    }
+
+    public static void errorCheck(int c) throws ArithmeticException {
+        if (c == 0) {
+            throw new ArithmeticException("На ноль делить нельзя! Это априори!");
+        }
     }
 }
